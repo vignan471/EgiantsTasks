9736a8d6b787a930385b72c72fbbe7629ae49b06
vignan471 <vignan.java@gmail.com>
2017-06-22 22:45:50 -0500

    SpringBoot RESTful

---
+++ crud-2/src/main/java/com/memorynotfound/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.memorynotfound.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.memorynotfound")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
