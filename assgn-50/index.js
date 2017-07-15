0ab2562ea33465c67dfef76eb57dd9169ac68360
vignan471 <vignan.java@gmail.com>
2017-07-15 15:10:03 -0500

    Spring Cloud

--- assgn-50/index.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>${process.env.MESSAGE}</h1>`);
-}).listen(8080);
